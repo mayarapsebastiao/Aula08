60f6e04e230e06ae25e972c9f04171310190a11c
Mayara Sebastiao <mayara.psebastiao@gmail.com>
2023-07-24 14:53:22 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,51 @@
+#tenho_sede = True
+
+#if tenho_sede:
+#    print("beber agua")
+
+#esta_frio = False
+
+#if esta_frio:
+#    print("vestir um casaco")
+#else:
+#    print("vestir camiseta")
+
+tenho_sede = True
+tenho_fome = False
+
+#if tenho_sede or tenho_fome:
+#    print("vou na cozinha")
+#else:
+#    print("ficar vendo netflix")
+
+#if tenho_sede and tenho_fome:
+#    print("fazer sanduiche e coca")
+#else:
+#    print("não tenho fome ou não tenho sede")    
+
+estou_em_dieta = True
+
+#if tenho_sede and tenho_fome:
+#    print("fazer sanduiche e coca")
+#elif tenho_sede and not(tenho_fome):
+#    if estou_em_dieta:
+#        print("tomar agua")
+#    else:
+#        print("tomar coca")
+#elif not(tenho_sede) and tenho_fome:
+#    print("fazer sanduiche")
+#else:
+#    print("ficar vendo netflix")  
+
+num1 = 5
+num2 = 32
+
+if num1 == num2:
+    print("num1 é igual num2")
+#elif num1 != num2:
+#    print("num1 é diferente de num2") 
+elif num1 > num2:
+    print("num1 é maior num2")   
+elif num1 < num2:
+    print("num1 é menor num2")   
+
